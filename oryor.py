c77e19cb013414d4294e27e5d62e0865a0bc0b53
diewland <diewland@gmail.com>
2018-09-26 17:02:27 +0700

    support look like oryor number

--- oryor.py
+++ oryor.py
@@ -7,17 +7,33 @@ from google.cloud import vision
 def find_oryor_no_from_img(path):
     client = vision.ImageAnnotatorClient()
 
+    # extract text from image
     with io.open(path, 'rb') as image_file:
         content = image_file.read()
     image = vision.types.Image(content=content)
     response = client.text_detection(image=image)
     texts = response.text_annotations
 
+    # find oryor no from text
     for text in texts:
-        match = re.search(r'\d{2}-\d-\d{5}-\d-\d{4}', text.description)
+        match = re.search(r'\d+-\d-\d+-\d-\d+', text.description)
         if match is not None:
-            return match.group()
-    return None
+            found = match.group()
+            match = re.search(r'\d{2}-\d-\d{5}-\d-\d{4}', found)
+            if match is not None:
+                return { # perfect oryor no
+                    'found': True,
+                    'oy_no': match.group()
+                }
+            else:
+                return { # look like oryor no
+                    'found': False,
+                    'oy_no': found
+                }
+    return { # not found oryor no
+        'found': False,
+        'oy_no': None
+    }
 
 def query_oryor_no_info(oryor_no):
     url = 'https://oryor.com/oryor2015/ajax-check-product.php'
@@ -26,10 +42,26 @@ def query_oryor_no_info(oryor_no):
     return obj['output'][0]
 
 def query_oryor_info_from_img(path):
-    oryor_no = find_oryor_no_from_img(path)
-    if oryor_no is not None:
-        return query_oryor_no_info(oryor_no)
-    return None
+    result = find_oryor_no_from_img(path)
+    if result['found']:
+        oy_no = result['oy_no']
+        return {
+            'success': True,
+            'oy_no': oy_no,
+            'data': query_oryor_no_info(oy_no),
+        }
+    elif result['oy_no'] is not None:
+        return {
+            'success': False,
+            'oy_no': result['oy_no'],
+            'data': None,
+        }
+    else: # not found
+        return {
+            'success': False,
+            'oy_no': None,
+            'data': None,
+        }
 
 if __name__ == "__main__":
 
